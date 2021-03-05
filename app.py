23a98fa01bb0b91b5b7e1193f5192c68459ab054
Daniyal A. Syed <50423149+daniyalas@users.noreply.github.com>
2021-03-05 18:09:14 +0500

    implemented caching and display of user_input
    1. Now the API will only be called when a new incoming_message is fetched. Otherwise, it will use data from cache
    2. Every time a user inputs somethings, they will see their own response displayed on the screen after a bolded "You: "

--- app.py
+++ app.py
@@ -1,11 +1,68 @@
 import streamlit as st
 import requests
 import json
+from streamlit.script_runner import RerunException
 
-# @st.cache
-def fetch_response(url, payload):
-    r = requests.post(url, data = payload)
-    return r
+# @st.cache(suppress_st_warning=True) # suppressess the write/markdown warning
+@st.cache
+def fetch_response(url, incoming_message):
+    payload = {
+        "user_id":1, 
+        "bot_id":1, 
+        "module_id":1, 
+        "channel":"Youtube", 
+        "incoming_message":incoming_message,
+        "step_id":1
+        }
+    response = requests.post(url, data = payload)
+    return response
+
+def show_all_results(results):
+    for result in results:
+        if len(results) > 1:
+            st.markdown('---') # This creates a thin divider
+
+        video_url = result['buttons'][0]['value']
+        st.video(
+            data = video_url,
+            # format = 'video/mp4',
+            # start_time = 0
+        )
+        st.markdown(f' **Video URL**: {video_url} ')
+
+        # handle Error for when API has been exhausted
+        if 'google.com/sorry/' in video_url:
+            st.write('*Oops!* The video can not be retreived at the moment. Please come back later and try again.')
+
+# Main function that runs when we get our desired input
+def handle_correct_reply(reply):
+    st.markdown('Perfect! Please wait for your customized playlist to load...')
+
+    r = fetch_response(url, reply) # 'r' stands for 'response'
+    
+    # # Uncomment for testing only:
+    # r
+    # r.url
+    # r.json()
+
+    json_data = r.json()
+    message = r.json()['message']
+    results = r.json()['cards']
+
+    st.markdown(f'''### {message}''')
+    st.markdown(f'*Number of results: {len(results)}*')
+
+    show_all_results(results)
+
+
+# --- CONFIGURATIONS
+keywords = ['nlp', 'ecommerce', 'robotics']
+input_counter = 0
+url = 'https://mhemani-youtube-bot.herokuapp.com/api_youtube/'
+nos = ['no', 'na', 'nah', 'n', 'nay']
+yeses = ['yes', 'yup', 'yeah', 'yo', 'ye', 'y']
+
+# ---
 
 st.title('Karachi.AI Tutor Chatbot')
 
@@ -21,13 +78,10 @@ For example: *Ecommerce*, *NLP* or even *Robotics*.
 I will create a customized playlist for you.
 ''')
 
-keywords = ['nlp', 'ecommerce', 'robotics']
-input_counter = 0
-
 try:
     while True:
         while True:
-            reply = st.text_input(
+            reply1 = st.text_input(
                 label = 'Type your search term here',
                 value = '',
                 # max_chars = None,
@@ -35,65 +89,26 @@ try:
                 # type = "default"
                 )
 
-            if reply != '':
-                st.markdown(f'**You:** {reply}')
-                reply = reply.lower()
-                if reply in keywords:
+            if reply1 != '':
+                st.markdown(f'**You:** {reply1}')
+                reply1 = reply1.lower()
+                if reply1 in keywords:
                     break # escape the current WHILE loop
 
-                st.markdown('**Oops!** It looks like our playlist for this is not ready yet. Try something else perhaps.')
+                st.markdown('''
+                *Oops!* It looks like our playlist for this is not ready yet. Try something else perhaps.
+
+                For example: *Ecommerce*, *NLP* or even *Robotics*.
+                ''')
                 break  # escape the current WHILE loop
 
-        if reply not in keywords:
+        if reply1 not in keywords:
             input_counter += 1
             continue # repeat the current WHILE loop
         else:
-        # Main program that runs when we get our desired input
-
-            st.markdown('Perfect! Please wait for your customized playlist to load...')
-
-            url = 'https://mhemani-youtube-bot.herokuapp.com/api_youtube/'            
-            payload = {
-                "user_id":1, 
-                "bot_id":1, 
-                "module_id":1, 
-                "channel":"Youtube", 
-                "incoming_message":reply,
-                "step_id":1
-            }
-
-            # response = r
-            r = fetch_response(url, payload)
-            # st.write(r.url) # Uncomment to verify whether correct URL is being sent
-
-            json_data = r.json()
-            # json_data # Uncomment to verify whether correct json_data is being received
-
-            message = r.json()['message']
-            results = r.json()['cards']
-
-            st.markdown(f'*Number of results: {len(results)}*')
-
-            for result in results:
-                if len(results) > 1:
-                    st.markdown('---') # This creates a thin divider
-
-                video_url = result['buttons'][0]['value']
-                st.video(
-                    data = video_url,
-                    # format = 'video/mp4',
-                    # start_time = 0
-                )
-                st.markdown(f' **Video URL**: {video_url} ')
-
-                # error handling for when API has been exhausted
-                if 'google.com/sorry/' in video_url:
-                    st.write('**Ooops!** The video can not be retreived at the moment. Please come back later and try again.')
+            handle_correct_reply(reply1)
             
             st.markdown('Do you want to search for a different kind of playlist?')
-
-            nos = ['no', 'na', 'nah', 'n', 'nay']
-            yeses = ['yes', 'yup', 'yeah', 'yo', 'ye', 'y']
 
             while True:
                 while True:
@@ -111,7 +126,7 @@ try:
                         if reply2 in nos or reply2 in yeses:
                             break # escape the current While loop
                         
-                        st.markdown('''**Oops!** I don't think I quite got that. Do you want to search for a different kind of playlist, Yes or No?''')
+                        st.markdown('''*Oops!* I don't think I quite got that. Do you want to search for a different kind of playlist, Yes or No?''')
                         input_counter += 1
                         continue
                 
@@ -120,15 +135,16 @@ try:
                     break # escape the current While loop
 
                 if reply2 in yeses:
-                    break # escape the current While loop
+                    break # escape the current WHILE loop
             
             if reply2 in yeses:
                 st.write('Alright then, what would you like to learn about?')
                 input_counter += 1
                 continue # repeat the current WHILE loop
-
-            break # escape the current While loop
+            
+            break # this ensures that another input box doesn't appear after a 'NO'
 except Exception as err:
+
     # # Use these to analyse the Error
     # st.write(type(err))    # the exception instance
     # st.write(err.args)     # arguments stored in .args
@@ -144,7 +160,7 @@ except Exception as err:
 st.markdown('---') # This creates a thin divider
 st.markdown('This streamlit app is developed by [Daniyal A. Syed](https://www.linkedin.com/in/daniyal-as/)')
 
-# The logic of this loop was used to create the WHOLE loop of the code
+# The logic of the following loop was used to create the WHOLE loop of the code
 # input_counter = 1
 # while True:
 #     # main program
